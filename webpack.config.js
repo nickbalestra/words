b4078ddbec4c3eade1d686ac5b94b9fa60d25c0f
Nick Balestra <nick@balestra.ch>
2015-12-13 20:38:19 -0800

    webpack config setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-  entry: './public/app.js',
+  entry: path.resolve(__dirname, 'app/app.js'),
   
   output: {
-    filename: 'public/bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'build.js'
   },
 
-  // module: {
-  //   loaders: [
-  //     {test: /\.js$/, loader: "babel", query: {presets:['react', 'es2015']}}
-  //   ]
-  // },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'ng-annotate' },
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: "url-loader?limit=1024&name=fonts/[name].[ext]" }
+    ]
+  },
 
   plugins: [
-    new webpack.ProvidePlugin({
-      'angular': 'angular'
-      // new webpack.optimize.CommonsChunkPlugin('public/shared.js'),
-    })
+    new ExtractTextPlugin("styles.css"),
+    new webpack.optimize.UglifyJsPlugin({mangle: false}),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
   ]
 };
