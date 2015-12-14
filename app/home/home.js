39fb3f9ef4ceea7d83bf03c6df6ba7118e080178
Nick Balestra <nick@balestra.ch>
2015-12-13 22:54:35 -0800

    gh pages deploy

--- app/home/home.js
+++
@@ -1,7 +0,0 @@
-module.exports = ($scope, $timeout, $state, Utils, $firebaseObject) => {
-  $scope.userName = Utils.user.name;
-  
-  $scope.startGame = () => {
-    Utils.startGame($state, $scope.userName);
-  };
-};
